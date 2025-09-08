9db991eb27d2344e9e06357739545ea702c4dc3c
ryoyamamura <ryo.bananadaiski@gmail.com>
2025-09-08 22:23:09 +0900

    feat: コンテナの起動スクリプトを記載

---
+++ restart-container.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IMAGE_NAME="llama-fastapi-app"
+
+docker rm -f "${IMAGE_NAME}-container" 2>/dev/null || true
+
+docker run -it -d \
+  --name="${IMAGE_NAME}-container" \
+  -p 8001:8080 \
+  -v ./app:/app \
+  ${IMAGE_NAME}:latest \
+  uvicorn main:app --host 0.0.0.0 --port 8080 --reload
